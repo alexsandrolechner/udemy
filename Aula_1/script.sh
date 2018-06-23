28c84ea8080f8bd46a310bb1ae14423984d1656b
franciscoed <franciscoed@gmail.com>
2018-06-23 22:17:02 +0100

    Bootstrap script for ec2 training

--- Aula_1/script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-sudo yum -y install httpd php
-sudo service httpd start
-sudo chkconfig httpd on
-cd /var/www/html
-sudo wget https://raw.githubusercontent.com/franciscoed/udemy/master/Aula_1/hostname.php
